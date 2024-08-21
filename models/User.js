020b246d8e7b5cca7d683a096233c88373b6e9cf
Angel Sanabria <asanabria-2021067@kinal.edu.gt>
2024-08-20 18:54:20 -0600

    changes in user model

--- models/User.js
+++ models/User.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  role: { type: String, required: true, enum: ["Usuario", "Administrador", "Conductor"] },
-  lastName: { type: String, required: true },
-  studentId: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  firstName: { type: String, required: false },
+  role: { type: String, required: false, enum: ["Usuario", "Administrador", "Conductor"] },
+  lastName: { type: String, required: false },
+  studentId: { type: String, required: false },
+  email: { type: String, required: false, unique: true },
+  password: { type: String, required: false },
   phone: { type: String , default: ""},
   location: {
     type: {
       type: String,
       enum: ['Point'],
-      required: true,
+      required: false,
       default: 'Point'
     },
     coordinates: {
       type: [Number],
-      required: true,
+      required: false,
       default: [0, 0],
       validate: {
         validator: function (value) {
