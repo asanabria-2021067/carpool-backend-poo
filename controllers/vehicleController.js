403bc3357c792b8436838003df5829755b14e0fd
Angel Sanabria <asanabria-2021067@kinal.edu.gt>
2024-08-27 16:46:28 -0600

    changes in updateVehicle

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -1,47 +1,52 @@
-const User = require('../models/User');
-const Vehicle = require('../models/Vehicle');
+const User = require("../models/User");
+const Vehicle = require("../models/Vehicle");
 
 // Crear vehículo
 const createVehicle = async (req, res) => {
   const id = req.usuario.id;
   const owner = User.findById(id);
   if (!owner) {
-    return res.status(400).json({ message: 'User not found' });
+    return res.status(400).json({ message: "User not found" });
   }
-  const { type, capacity, plateNumber, model, carImage  } = req.body;
-    const vehicle = await Vehicle.create({
-      owner: id,
-      type: type,
-      capacity: capacity,
-      plateNumber: plateNumber,
-      model: model,
-      carImage: carImage,
-    });
+  const { type, capacity, plateNumber, model, carImage } = req.body;
+  const vehicle = await Vehicle.create({
+    owner: id,
+    type: type,
+    capacity: capacity,
+    plateNumber: plateNumber,
+    model: model,
+    carImage: carImage,
+  });
 
-    res.status(201).json(vehicle);
+  res.status(201).json(vehicle);
 };
 
 const getAllVehicles = async (req, res) => {
-    const vehicle = await Vehicle.find().populate('owner', 'firstName lastName email');
-    if (!vehicle) {
-      return res.status(404).json({ message: 'Vehicle not found' });
-    }
-    res.json(vehicle);
+  const vehicle = await Vehicle.find().populate(
+    "owner",
+    "firstName lastName email"
+  );
+  if (!vehicle) {
+    return res.status(404).json({ message: "Vehicle not found" });
+  }
+  res.json(vehicle);
 };
-
 
 // Obtener vehículo por ID
 const getVehicleById = async (req, res) => {
   const id = req.usuario.id;
   const owner = User.findById(id);
   try {
-    const vehicle = await Vehicle.find({owner: id}).populate('owner', 'firstName lastName email');
+    const vehicle = await Vehicle.find({ owner: id }).populate(
+      "owner",
+      "firstName lastName email"
+    );
     if (!vehicle) {
-      return res.status(404).json({ message: 'Vehicle not found' });
+      return res.status(404).json({ message: "Vehicle not found" });
     }
     res.json(vehicle);
   } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 };
 
@@ -50,12 +55,11 @@ const updateVehicle = async (req, res) => {
   const { type, capacity, plateNumber } = req.body;
   const id = req.usuario.id;
   const owner = User.findById(id);
-  try {
-    if(owner){
-    const vehicle = await Vehicle.findOne({owner: id});
+  if (owner) {
+    const vehicle = await Vehicle.findOne({ owner: id });
 
     if (!vehicle) {
-      return res.status(404).json({ message: 'Vehicle not found' });
+      return res.status(404).json({ message: "Vehicle not found" });
     }
 
     vehicle.type = type || vehicle.type;
@@ -66,11 +70,8 @@ const updateVehicle = async (req, res) => {
     await vehicle.save();
 
     res.json(vehicle);
-  }else{
-    return res.status(403).json({ message: 'Vehicle not found' });
-  }
-  } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+  } else {
+    return res.status(403).json({ message: "Vehicle not found" });
   }
 };
 
@@ -78,17 +79,17 @@ const updateVehicle = async (req, res) => {
 const deleteVehicle = async (req, res) => {
   const id = req.usuario.id;
   try {
-    const vehicle = await Vehicle.findOne({owner: id});
+    const vehicle = await Vehicle.findOne({ owner: id });
 
     if (!vehicle) {
-      return res.status(404).json({ message: 'Vehicle not found' });
+      return res.status(404).json({ message: "Vehicle not found" });
     }
 
     await vehicle.remove();
 
-    res.json({ message: 'Vehicle removed' });
+    res.json({ message: "Vehicle removed" });
   } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 };
 
