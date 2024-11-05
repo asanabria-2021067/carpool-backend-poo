de5a9c914578de17c1851251f44aaa8d0d407f42
Angel Sanabria <asanabria-2021067@kinal.edu.gt>
2024-11-04 22:57:37 -0600

    changes in radar

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,7 +13,7 @@ router.post('/get', getAllUsers);
 router.post('/sendCode',[protect], sendSecurityCode);
 router.post('/update', [protect], updateMyProfile);
 router.post('/join', [protect], joinTrip);
-router.get('/radar', [protect], radarLocation);
+router.post('/radar', [protect], radarLocation);
 router.get('/updateLocation', updateUserLocation);
 router.post('/accept',  [protect], acceptPassenger);
 router.post('/cancel/passenger',  [protect], cancelPassenger);
